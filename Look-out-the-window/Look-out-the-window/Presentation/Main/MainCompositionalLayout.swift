09226b1b9aa6d9775d5e946559b633cd7bdff007
UkhyunGo <dnrwk99@gmail.com>
2025-05-20 20:02:31 +0900

    feat: Main화면 Compositional Layout 구현

--- Look-out-the-window/Look-out-the-window/Presentation/Main/MainCompositionalLayout.swift
+++ Look-out-the-window/Look-out-the-window/Presentation/Main/MainCompositionalLayout.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 enum Section: Int, CaseIterable {
-    case hourlyForecast /// 시간별 예보
-    case dailyForecast /// 일별 예보
-    case detailForecast /// 상세 정보
+    case hourly /// 시간별 예보
+    case daily /// 일별 예보
+    case detail /// 상세 정보
 }
 
 struct MainCompositionalLayout {
@@ -19,124 +19,75 @@ struct MainCompositionalLayout {
             section,
             _ in
             switch Section(rawValue: section) {
-            case .hourlyForecast:
+            case .hourly:
                 
-                let itemSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(80)
-                )
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .fractionalWidth(1.0))
                 
                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
                 
-                let groupSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(0.5),
-                    heightDimension: .absolute(80)
-                )
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.22),
+                                                       heightDimension: .absolute(80))
                 
-                let group = NSCollectionLayoutGroup.horizontal(
-                    layoutSize: groupSize,
-                    subitems: [item]
-                )
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                               subitems: [item])
                 
                 let section = NSCollectionLayoutSection(group: group)
                 
-                let headerSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(44)
-                )
-                let header = NSCollectionLayoutBoundarySupplementaryItem(
-                    layoutSize: headerSize,
-                    elementKind: UICollectionView.elementKindSectionHeader,
-                    alignment: .top
-                )
-                section.boundarySupplementaryItems = [header]
-                
                 section.interGroupSpacing = 10
-                section.orthogonalScrollingBehavior = .continuous
-                section.contentInsets = NSDirectionalEdgeInsets(
-                    top: 0,
-                    leading: 16,
-                    bottom: 20,
-                    trailing: 16
-                )
+                section.orthogonalScrollingBehavior = .continuous // 가로 스크롤
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                                leading: 16,
+                                                                bottom: 8,
+                                                                trailing: 16)
                 return section
                 
-            case .dailyForecast:
-                let itemSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(80)
-                )
+            case .daily:
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .fractionalWidth(1.0))
                 
                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
                 
-                let groupSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(80)
-                )
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .absolute(64))
                 
-                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                             subitems: [item])
                 
                 let section = NSCollectionLayoutSection(group: group)
                 
-                let headerSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(44)
-                )
-                let header = NSCollectionLayoutBoundarySupplementaryItem(
-                    layoutSize: headerSize,
-                    elementKind: UICollectionView.elementKindSectionHeader,
-                    alignment: .top
-                )
-                section.boundarySupplementaryItems = [header]
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                        heightDimension: .absolute(44))
                 
                 section.interGroupSpacing = 10
-                section.contentInsets = NSDirectionalEdgeInsets(
-                    top: 0,
-                    leading: 16,
-                    bottom: 20,
-                    trailing: 16
-                )
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                                leading: 16,
+                                                                bottom: 8,
+                                                                trailing: 16)
                 
                 return section
                 
-            case .detailForecast:
-                let itemSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(80)
-                )
-                
+            case .detail: // 2열 그리드 형태
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .fractionalHeight(1.0))
                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                
-                let groupSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(80)
-                )
-                
-                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-                
+                item.contentInsets = NSDirectionalEdgeInsets(top: 8,
+                                                             leading: 8,
+                                                             bottom: 8,
+                                                             trailing: 8)
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .absolute(120))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                               repeatingSubitem: item,
+                                                               count: 2)
                 let section = NSCollectionLayoutSection(group: group)
-                
-                let headerSize = NSCollectionLayoutSize(
-                    widthDimension: .fractionalWidth(1.0),
-                    heightDimension: .absolute(44)
-                )
-                let header = NSCollectionLayoutBoundarySupplementaryItem(
-                    layoutSize: headerSize,
-                    elementKind: UICollectionView.elementKindSectionHeader,
-                    alignment: .top
-                )
-                section.boundarySupplementaryItems = [header]
-                
-                section.interGroupSpacing = 10
-                section.contentInsets = NSDirectionalEdgeInsets(
-                    top: 0,
-                    leading: 16,
-                    bottom: 20,
-                    trailing: 16
-                )
-                
+                section.interGroupSpacing = 8
+                section.contentInsets = NSDirectionalEdgeInsets(top: 0,
+                                                                leading: 16,
+                                                                bottom: 8,
+                                                                trailing: 16)
                 return section
-            
+                
             default:
                 return nil
             }
