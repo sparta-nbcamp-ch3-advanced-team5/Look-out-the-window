b14402b2eea2b98e38e2bd7fb45b94fb77ae46ba
EllGHT <rmsgh1188@gmail.com>
2025-05-20 19:27:59 +0900

    feat: 뷰 틀 구현

--- Look-out-the-window/Look-out-the-window/Presentation/Main/VIewController/BackgroundViewController.swift
+++ Look-out-the-window/Look-out-the-window/Presentation/Main/VIewController/BackgroundViewController.swift
@@ -14,10 +14,30 @@ import Then
 
 final class BackgroundViewController: UIViewController {
     
+    // MARK: - UI Components
+    private lazy var backgroundView = BackgroundView()
+    
+    private lazy var locationButton = UIButton().then {
+        // 버튼의 SFSymbol 이미지 크기 변경 시 사용
+//        let config = UIImage.SymbolConfiguration(pointSize: 44, weight: .regular)
+        $0.setImage(UIImage(systemName: "location.fill", withConfiguration: nil), for: .normal)
+        $0.tintColor = .label
+        $0.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    private lazy var listButton = UIButton().then {
+//        let config = UIImage.SymbolConfiguration(pointSize: 44, weight: .regular)
+        $0.setImage(UIImage(systemName: "list.bullet", withConfiguration: nil), for: .normal)
+        $0.tintColor = .label
+        $0.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    // MARK: - Initializers
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .systemBackground
+        setupUI()
+        bind()
         
         guard let apiKeyEncoding = Bundle.main.object(forInfoDictionaryKey: "API_KEY_ENCODING") as? String,
               let apiKeyDecoding = Bundle.main.object(forInfoDictionaryKey: "API_KEY_DECODING") as? String,
@@ -27,5 +47,32 @@ final class BackgroundViewController: UIViewController {
         print(apiKeyDecoding)
         print(clientId)
         print(clientSecret)
+    }
+    
+    // MARK: - UI & Layout
+    private func setupUI() {
+        view.addSubviews(backgroundView, locationButton, listButton)
+        
+        backgroundView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        locationButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.width.height.equalTo(44)
+        }
+        
+        listButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.width.height.equalTo(44)
+        }
+    }
+    
+    
+    // MARK: - Private Methods
+    private func bind() {
+        backgroundView.rx.tapGesture()
     }
 }
--- Look-out-the-window/Look-out-the-window/Presentation/Main/View/BackgroundView.swift
+++ Look-out-the-window/Look-out-the-window/Presentation/Main/View/BackgroundView.swift
@@ -11,14 +11,75 @@ import SnapKit
 import Then
 
 final class BackgroundView: UIView {
+        
+    // MARK: - UI Components
+    private lazy var infoStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .equalSpacing
+        $0.alignment = .center
+        $0.spacing = 8
+    }
+    
+    private lazy var city = UILabel().then {
+        $0.text = "부산광역시"
+        $0.numberOfLines = 0
+        $0.font = .systemFont(ofSize: 30, weight: .medium)
+        $0.textColor = .label
+    }
+    
+    private lazy var temperature = UILabel().then {
+        $0.text = "20°"
+        $0.font = .systemFont(ofSize: 90, weight: .light)
+        $0.textColor = .label
+    }
+    
+    private lazy var weather = UILabel().then {
+        $0.text = "흐림"
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.textColor = .secondaryLabel
+    }
+    
+    private lazy var tempStackView = UIStackView().then() {
+        $0.axis = .horizontal
+        $0.distribution = .equalSpacing
+        $0.spacing = 8
+    }
+    
+    private lazy var highestTemp = UILabel().then {
+        $0.text = "H:24°"
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.textColor = .label
+    }
+    
+    private lazy var lowestTemp = UILabel().then {
+        $0.text = "L:18°"
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.textColor = .label
+    }
+    
     
     // MARK: - Initializer
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = UIColor(named: "MainBackground")
+        
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - UI & Layout
+    private func setupUI() {
+        self.addSubviews(infoStackView)
+        infoStackView.addArrangedSubviews(city, temperature, weather, tempStackView)
+        tempStackView.addArrangedSubviews(highestTemp, lowestTemp)
+        
+        infoStackView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide).inset(UIScreen().bounds.height / 10.0)
+            $0.centerX.equalToSuperview()
+        }
     }
 }
