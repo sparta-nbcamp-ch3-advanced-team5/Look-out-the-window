5e70045b96730223ac61a083cdf907c5e6056697
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-05-21 15:33:58 +0900

    feat: #5 - 위치 권한 요청 코드 추가

--- Look-out-the-window/Look-out-the-window/Presentation/Shared/CoreLocation/CoreLocationManager.swift
+++ Look-out-the-window/Look-out-the-window/Presentation/Shared/CoreLocation/CoreLocationManager.swift
@@ -7,19 +7,91 @@
 
 import Foundation
 import CoreLocation
+import OSLog
 
+/// `CoreLocation`의 싱글톤 매니저
 final class CoreLocationManager: NSObject {
     
-    static let shared: CoreLocationManager = {
-        let instance = CoreLocationManager()
-        locationManager.delegate = instance
-        
-        return instance
-    }()
+    private lazy var log = OSLog(subsystem: Bundle.main.bundleIdentifier!, category: "CoreLocationManager")
     
-    static let locationManager = CLLocationManager()
+    // MARK: - Properties
+    
+    static let shared = CoreLocationManager()
+    
+    private let locationManager: CLLocationManager
+    
+    // MARK: - Initializer
+    private override init() {
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        super.init()
+        
+        locationManager.delegate = self
+    }
+    
+    // MARK: - Methods
+    
+    func startUpdatingLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
 }
 
-extension CoreLocationManager: CLLocationManagerDelegate {
+// MARK: - Private Methods
+
+private extension CoreLocationManager {
+    /// 디바이스 위치 서비스가 활성화 상태인지 확인
+    func checkDeviceLocationService() {
+        Task.detached { [weak self] in
+            guard let self else { return }
+            if CLLocationManager.locationServicesEnabled() {
+                os_log(.debug, log: self.log, "디바이스 위치 서비스: On")
+                
+                let status = locationManager.authorizationStatus
+                checkUserLocationServiceAuthorization(status: status)
+            } else {
+                os_log(.debug, log: self.log, "디바이스 위치 서비스: Off")
+            }
+        }
+    }
     
+    /// 앱 위치 접근 권한 확인
+    func checkUserLocationServiceAuthorization(status: CLAuthorizationStatus) {
+        let status = locationManager.authorizationStatus
+        switch status {
+        case .authorizedAlways:
+            os_log(.debug, log: log, "위치 서비스 권한: 항상 허용됨")
+            startUpdatingLocation()
+        case .authorizedWhenInUse:
+            os_log(.debug, log: log, "위치 서비스 권한: 사용하는 동안 허용됨")
+            startUpdatingLocation()
+            // TODO: 항상 허용 Alert?
+        case .restricted, .denied:
+            os_log(.debug, log: log, "위치 서비스 권한: 차단됨")
+        case .notDetermined:
+            os_log(.debug, log: log, "위치 서비스 권한: 설정 필요")
+            locationManager.requestAlwaysAuthorization()
+        @unknown default:
+            break
+        }
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension CoreLocationManager: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkDeviceLocationService()
+        print("locationManagerDidChangeAuthorization")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let coord = locations.last?.coordinate else { return }
+        os_log(.debug, log: log, "lat: \(coord.latitude), lng: \(coord.longitude)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        
+    }
 }
