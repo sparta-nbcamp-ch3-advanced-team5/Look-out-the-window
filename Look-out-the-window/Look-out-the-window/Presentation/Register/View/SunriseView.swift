a94484ce9ff1afd3a819be90142bad57dde0490d
minjae <qlqlzbto@naver.com>
2025-05-23 00:49:07 +0900

    feat: #15 - 시간, 시간마크 레이블 생성 및 레이아웃 구성

--- Look-out-the-window/Look-out-the-window/Presentation/Register/View/SunriseView.swift
+++ Look-out-the-window/Look-out-the-window/Presentation/Register/View/SunriseView.swift
@@ -6,15 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SunriseView: UIView {
     
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "6:23"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 32, weight: .regular)
+        return label
+    }()
+    
+    private let timeMarkLabel: UILabel = {
+        let label = UILabel()
+        label.text = "AM"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = .systemBlue
+        setupUI()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+private extension SunriseView {
+    func setupUI() {
+        self.addSubviews(timeLabel, timeMarkLabel)
+        configureLayout()
+    }
+    
+    func configureLayout() {
+        timeLabel.snp.makeConstraints {
+            $0.leading.top.equalToSuperview().inset(5)
+        }
+        timeMarkLabel.snp.makeConstraints {
+            $0.leading.equalTo(timeLabel.snp.trailing)
+            $0.top.equalToSuperview().inset(16)
+        }
+    }
+    
 }
