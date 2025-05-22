51499b3ea9c9809a9408ed451c4271ba8d2a0149
minjae <qlqlzbto@naver.com>
2025-05-23 00:26:23 +0900

    feat: #15 - SunriseView 생성

---
+++ Look-out-the-window/Look-out-the-window/Presentation/Register/View/SunriseView.swift
@@ -0,0 +1,20 @@
+//
+//  SunriseView.swift
+//  Look-out-the-window
+//
+//  Created by MJ Dev on 5/23/25.
+//
+
+import UIKit
+
+final class SunriseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
