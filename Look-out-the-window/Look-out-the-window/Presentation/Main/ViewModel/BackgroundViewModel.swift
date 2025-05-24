661d515a94bffcea0fb0301f0d1ca66aac3d1b5a
EllGHT <rmsgh1188@gmail.com>
2025-05-24 16:11:57 +0900

    feat: #22 - BackgroundTopInfoView, MainView 통합
    추후 리팩토링 필요

--- Look-out-the-window/Look-out-the-window/Presentation/Main/ViewModel/BackgroundViewModel.swift
+++ Look-out-the-window/Look-out-the-window/Presentation/Main/ViewModel/BackgroundViewModel.swift
@@ -52,7 +52,10 @@ final class BackgroundViewModel: ViewModelProtocol {
     // MARK: - Initializer
     
     init() {
-        self.urlRequest = APIEndpoints.getURLRequest(APIEndpoints.weather, parameters: WeatherParameters(lat: currentLocation.lat, lng: currentLocation.lng, appid: Bundle.main.infoDictionary?["API_KEY"] as? String ?? "").makeParameterDict())
+        self.urlRequest = APIEndpoints.getURLRequest(APIEndpoints.weather, parameters: WeatherParameters(
+            lat: currentLocation.lat,
+            lng: currentLocation.lng,
+            appid: Bundle.main.infoDictionary?["API_KEY"] as? String ?? "").makeParameterDict())
         state.actionSubject
             .subscribe(with: self) { owner, action in
                 switch action {
