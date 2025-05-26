7b052d60579b61c2e060c3b089918c18453814d9
minjae <qlqlzbto@naver.com>
2025-05-26 22:42:20 +0900

    feat: #40 - RegisterVC, RegisterVM 생성

---
+++ Look-out-the-window/Look-out-the-window/Presentation/Register/ViewController/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  Look-out-the-window
+//
+//  Created by MJ Dev on 5/26/25.
+//
+
+import UIKit
+
+final class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Look-out-the-window/Look-out-the-window/Presentation/Register/ViewModel/RegisterViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterViewModel.swift
+//  Look-out-the-window
+//
+//  Created by MJ Dev on 5/26/25.
+//
+
+import Foundation
+
+final class RegisterViewModel {
+    
+}
