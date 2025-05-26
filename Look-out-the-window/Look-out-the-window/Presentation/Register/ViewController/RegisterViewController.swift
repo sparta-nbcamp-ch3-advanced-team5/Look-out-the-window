f5519d3f5f191593d66a0ce793d9fd747f0e0185
minjae <qlqlzbto@naver.com>
2025-05-27 03:47:48 +0900

    feat: #40 - RegisterVC 구현 / 뷰모델과 바인딩

--- Look-out-the-window/Look-out-the-window/Presentation/Register/ViewController/RegisterViewController.swift
+++ Look-out-the-window/Look-out-the-window/Presentation/Register/ViewController/RegisterViewController.swift
@@ -7,9 +7,75 @@
 
 import UIKit
 
+import SnapKit
+import Then
+import RxSwift
+import RxDataSources
+import RxRelay
+
 final class RegisterViewController: UIViewController {
+    
+    let detailView = WeatherDetailCollectionView()
+    let disposeBag = DisposeBag()
+    let viewModel: RegisterViewModel
+    
+    private let addButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "plus"), for: .normal)
+    }
+    
+    init(viewModel: RegisterViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        
+        
+        bind()
+    }
+    
+    func bind() {
+        viewModel.action.onNext(.viewDidLoad)
+        
+        viewModel.state.currentWeather
+            .compactMap{ $0 }
+            .bind(to: detailView.rx.items(dataSource: detailView.detailDataSource))
+            .disposed(by: disposeBag)
+        
+        addButton.rx.tap
+            .subscribe(with: self) { owner, _ in
+                owner.viewModel.action.onNext(.plusButtonTapped)
+            }.disposed(by: disposeBag)
     }
 }
+
+private extension RegisterViewController {
+    func setupUI() {
+        setNavigationBar()
+        addViews()
+        configureLayout()
+    }
+    
+    func setNavigationBar() {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: addButton)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismiss))
+    }
+    
+    func addViews() {
+        view.addSubviews(detailView)
+    }
+    
+    func configureLayout() {
+        detailView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
