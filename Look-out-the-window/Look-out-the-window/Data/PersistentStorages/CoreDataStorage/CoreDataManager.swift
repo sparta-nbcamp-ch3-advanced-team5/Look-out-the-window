e32ea97f8723b402f9fe24cc54363b79d8852488
YunJuHyung <wngud5289@naver.com>
2025-05-27 16:56:30 +0900

    feat: #38 CoreData 주소값으로 업데이트 삭제 메서드 구현

--- Look-out-the-window/Look-out-the-window/Data/PersistentStorages/CoreDataStorage/CoreDataManager.swift
+++ Look-out-the-window/Look-out-the-window/Data/PersistentStorages/CoreDataStorage/CoreDataManager.swift
@@ -35,10 +35,10 @@ final class CoreDataManager {
     }
 
     // MARK: create 주형: 수정할 부분 위도,경도 param 필요없어짐
-    func saveWeatherData(current: CurrentWeather, latitude: Double, longitude: Double) {
+    func saveWeatherData(current: CurrentWeather) {
         let weather = WeatherDataEntity(context: context)
-        weather.latitude = latitude
-        weather.longitude = longitude
+        weather.latitude = current.lat
+        weather.longitude = current.lng
         weather.address = current.address
         weather.temperature = current.temperature
         weather.currentTime = Int64(current.currentTime)
@@ -89,6 +89,71 @@ final class CoreDataManager {
         }
     }
 
+    //MARK: update 주소값을 받아서 업데이트
+    func updateWeather(for address: String, with updateWeather: CurrentWeather) {
+        let request: NSFetchRequest<WeatherDataEntity> = WeatherDataEntity.fetchRequest()
+        request.predicate = NSPredicate(format: "address == %@", address)
+
+        do {
+            if let entity = try context.fetch(request).first {
+                entity.temperature = updateWeather.temperature
+                entity.maxTemp = updateWeather.maxTemp
+                entity.minTemp = updateWeather.minTemp
+                entity.tempFeelLike = updateWeather.tempFeelLike
+                entity.skyInfo = updateWeather.skyInfo
+                entity.pressure = updateWeather.pressure
+                entity.humidity = updateWeather.humidity
+                entity.clouds = updateWeather.clouds
+                entity.uvi = updateWeather.uvi
+                entity.visibility = updateWeather.visibility
+                entity.windSpeed = updateWeather.windSpeed
+                entity.windDeg = updateWeather.windDeg
+                entity.rive = updateWeather.rive
+                entity.currentTime = Int64(updateWeather.currentTime)
+                entity.currentMomentValue = updateWeather.currentMomentValue
+                entity.sunriseTime = Int64(updateWeather.sunriseTime)
+                entity.sunsetTime = Int64(updateWeather.sunsetTime)
+                try context.save()
+            }
+            else {
+                print("update 되는 데이터 없음")
+            }
+        } catch {
+            print("주소 기반 fetch 실패: \(error.localizedDescription)")
+
+        }
+    }
+
+    //MARK: 주소값을 받아서 삭제하는 메서드
+    func deleteWeather(for address: String) {
+        let request: NSFetchRequest<WeatherDataEntity> = WeatherDataEntity.fetchRequest()
+        request.predicate = NSPredicate(format: "address == %@", address)
+
+        do {
+            if let objectToDelete = try context.fetch(request).first {
+                context.delete(objectToDelete)
+                try context.save()
+            }else {
+                print("삭제할 데이터 없음")
+            }
+        } catch {
+            print("주소 기반 fetch 실패: \(error.localizedDescription)")
+        }
+    }
+
+    func fetchWeather(for address: String) -> WeatherDataEntity? {
+        let request: NSFetchRequest<WeatherDataEntity> = WeatherDataEntity.fetchRequest()
+        request.predicate = NSPredicate(format: "address == %@", address)
+        request.fetchLimit = 1
+
+        do {
+            return try context.fetch(request).first
+        } catch {
+            print("주소 기반 fetch 실패: \(error.localizedDescription)")
+            return nil
+        }
+    }
+
 
     //MARK: fetch
     func fetchWeatherData() -> [WeatherDataEntity] {
@@ -119,7 +184,7 @@ final class CoreDataManager {
 
     // deleteAll
     func deleteAll() {
-        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = WeatherDataEntity.fetchRequest()
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "WeatherDataEntity")
         let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
 
         do {
@@ -127,22 +192,6 @@ final class CoreDataManager {
             try context.save()
         } catch {
             print("\(error.localizedDescription)")
-        }
-    }
-
-    //주소 기반 fetch
-    func fetchWeather(for address: String?) -> WeatherDataEntity? {
-        guard let address = address else { return nil }
-
-        let request: NSFetchRequest<WeatherDataEntity> = WeatherDataEntity.fetchRequest()
-        request.predicate = NSPredicate(format: "address == %@", address)
-        request.fetchLimit = 1
-
-        do {
-            return try context.fetch(request).first
-        } catch {
-            print("주소 기반 fetch 실패: \(error.localizedDescription)")
-            return nil
         }
     }
 
@@ -162,7 +211,6 @@ final class CoreDataManager {
 
 extension WeatherDataEntity {
 
-
     // 시간별 날씨 정렬
     var sortedHourlyArray: [HourlyWeatherEntity] {
         guard let set = hourly as? Set<HourlyWeatherEntity> else { return [] }
@@ -179,4 +227,59 @@ extension WeatherDataEntity {
         print("DailySetsorted가 정렬 되었는지 확인: \(DailySetsorted)")
         return DailySetsorted
     }
+    /// CurrentWeather 객체 생성 메서드
+    func toCurrentWeatherModel() -> CurrentWeather {
+        return CurrentWeather(
+            address: self.address ?? "--",
+            lat: self.latitude,
+            lng: self.longitude,
+            currentTime: Int(self.currentTime),
+            currentMomentValue: self.currentMomentValue,
+            sunriseTime: Int(self.sunriseTime),
+            sunsetTime: Int(self.sunsetTime),
+            temperature: self.temperature ?? "--",
+            maxTemp: self.maxTemp ?? "--",
+            minTemp: self.minTemp ?? "--",
+            tempFeelLike: self.tempFeelLike ?? "--",
+            skyInfo: self.skyInfo ?? "--",
+            pressure: self.pressure ?? "--",
+            humidity: self.humidity ?? "--",
+            clouds: self.clouds ?? "--",
+            uvi: self.uvi ?? "--",
+            visibility: self.visibility ?? "--",
+            windSpeed: self.windSpeed ?? "--",
+            windDeg: self.windDeg ?? "--",
+            rive: self.rive ?? "",
+            hourlyModel: self.sortedHourlyArray.map { $0.toModel() },
+            dailyModel: self.sortedDailyArray.map { $0.toModel() },
+            isCurrLocation: self.isCurrLocation
+        )
+    }
+
 }
+
+extension HourlyWeatherEntity {
+    func toModel() -> HourlyModel {
+        return HourlyModel(
+            hour: Int(self.time ?? "0") ?? 0,
+            temperature: self.temperature ?? "--",
+            weatherInfo: self.skyInfo ?? "--"
+        )
+    }
+}
+
+extension DailyWeatherEntity {
+    func toModel() -> DailyModel {
+        return DailyModel(
+            unixTime: Int(self.currentTime),
+            day: self.day ?? "--",
+            high: self.maxTemp ?? "--",
+            low: self.minTemp ?? "--",
+            weatherInfo: self.skyInfo ?? "--"
+        )
+    }
+}
+
+
+
+
